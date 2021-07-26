7f58bc274053196b6cd120263bbfc11eb37a0858
Jaishree Bala <jaishree1bala@gmail.com>
2021-07-25 23:46:29 -0400

    Updated navbar

--- client/src/components/Filters.js
+++ client/src/components/Filters.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Filters() {
-  return <div className="filters">Filters</div>;
+  return (
+    <div className="filters">
+      <h1>Filters</h1>
+    </div>
+  );
 }
 
 export default Filters;
