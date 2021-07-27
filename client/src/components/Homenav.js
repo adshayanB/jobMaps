31dc290d3f5a0d7a7899b8968ada838282b2cf48
Jaishree Bala <jaishree1bala@gmail.com>
2021-07-26 23:50:41 -0400

    Style filters

--- client/src/components/Homenav.js
+++ client/src/components/Homenav.js
@@ -8,7 +8,7 @@ function Homenav() {
         <ul>
           <li className="selected">Job Applications</li>
           <li>Dashboard</li>
-          <li>Calender</li>
+          <li>Calendar</li>
           <li>Logout</li>
         </ul>
       </div>
