cf66dd4e071270f7aa96be202475e0c495ed4433
Sayma Khan <saymakhan@Saymas-MacBook-Pro.local>
2021-09-05 13:30:13 -0400

    show interviews in calendar

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+const moment = require("moment");
 
+function Calendar({ setImportantDates }) {
+  const showImportantDates = async () => {
 
+    let token = localStorage.getItem("token");
+    let auth = "JWT " + token;
+    let calendarEvents = ["interview"];
+    let results = [];
+    let allEventTitles = []; 
+    let allEventDates = [];
+    let allEventsDisplay = []; 
 
+    for (const status of calendarEvents) { //get all interviews
+      try {
+        const response = await fetch("/filter/status", {
+          method: "POST",
+          headers: {
+            Authorization: auth,
+            "Content-type": "application/json; charset=UTF-8",
+          },
+          body: JSON.stringify({
+            status: status,
+          }),
+        });
+        const data = await response.json();
+        if (data.success) {
+          results.push(...data.data);
+          console.log(results);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
 
-function Calendar() {
+    for (const events in results) { //two arrays for two needed properties (event names and dates)
+      allEventDates.push(results[events].date_interview);
+
+      allEventTitles.push(
+        "Interview for " +
+          results[events].jobTitle +
+          " at " +
+          results[events].company
+      );
+    }
+
+    for (const events in results) { //combined the two arrays to make array of event objects for fullcalendar
+      var display_date = allEventDates[events];
+      var display_title = allEventTitles[events];
+
+      var insertEvents = {};
+      insertEvents = {
+        title: display_title,
+        start: moment(display_date).format("YYYY-MM-DD"),
+      };
+      allEventsDisplay[events]=(insertEvents); 
+    }
+    console.log(allEventsDisplay);
+
+    return allEventsDisplay;
+  };
+
   return (
     <div className="homeStyles">
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        height= "85%"
+        height="85%"
+        disableDragging="true"
         initialView="dayGridMonth"
         headerToolbar={{
-          center: 'dayGridMonth,timeGridWeek,timeGridDay',
+          center: "dayGridMonth,timeGridWeek,timeGridDay",
         }}
         eventColor="#38b6a1"
-        events={[
-            { title: 'test event 1!', date: '2021-08-06' },
-          ]}        
+        displayEventTime="false"
+        events={showImportantDates}
       />
     </div>
   );
